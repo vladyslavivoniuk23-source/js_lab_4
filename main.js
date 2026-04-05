fa0ac23a3ac3eb91a0a301d9081ec2f93368f83e
vladyslavivoniuk23-source <vladyslav.ivoniuk.23@pnu.edu.ua>
2026-04-06 01:45:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ function generateArray(size) {
 const baseArr = generateArray(100);
 
 console.log("%c ТЕСТ 1: Нерозріджений масив (100 елементів) ");
-MySortLib.bubbleSort([...baseArr]);
-MySortLib.selectionSort([...baseArr]);
-MySortLib.insertionSort([...baseArr]);
-MySortLib.shellSort([...baseArr]);
-MySortLib.quickSort([...baseArr]);
+SortLib.bubbleSort([...baseArr]);
+SortLib.selectionSort([...baseArr]);
+SortLib.insertionSort([...baseArr]);
+SortLib.shellSort([...baseArr]);
+SortLib.quickSort([...baseArr]);
 
 console.log("%c ТЕСТ 2: Розріджений масив (100 елементів) ", "background: #222; color: #ffcc00");
 
 let sparseArr = generateArray(50);
 sparseArr.length = 100; 
 
-MySortLib.quickSort(sparseArr, true);
+SortLib.quickSort(sparseArr, true);
 console.log("Вигляд масиву після сортування (undefined у кінці):", sparseArr);
